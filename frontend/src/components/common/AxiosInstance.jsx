306c5bf3fbfafe1f0ef061ef9d1fed28042f82db
sumit kumar <sumitkumar@sumits-MacBook-Air.local>
2024-11-06 11:23:01 +0530

    backurl

--- frontend/src/components/common/AxiosInstance.jsx
+++ frontend/src/components/common/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: 'https://studyapp-naan-mudhalvan-project.onrender.com', 
 });
 
 export default axiosInstance;
